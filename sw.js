63bee559f19e80ed89a9de5f578d8b482ed039d6
18551709007-debug <18551709007@qq.com>
2026-08-26 09:04:51 +0800

    更新单页铺满版离线缓存

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 "use strict";
-const CACHE_NAME = "tong-night-plan-8fc7cdf65c09";
+const CACHE_NAME = "tong-night-plan-3113cf2dd0d7";
 const APP_FILES = ["./", "./index.html", "./manifest.webmanifest", "./apple-touch-icon.png", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_FILES)).then(() => self.skipWaiting()));
